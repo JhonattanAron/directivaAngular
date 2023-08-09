c7af378d7a900ddb3fff0e78eea9a29f71f1fa9d
Aron <aron.cachago@gmail.com>
2023-08-08 21:14:00 -0500

    Directiva Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivaAngular';
+  protected titulo = 'Registro de Usuario';
+  protected mensaje = "Data Inicial"
+  protected registrado = false;
+  protected nombre:string = "";
+  protected apellido:string = "";
+
+
+  protected registrarUsuario(){
+    this.registrado = true;
+    this.mensaje = "Usuario Registrado Con Exito";
+  }
 }
