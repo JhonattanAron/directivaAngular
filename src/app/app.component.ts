ea6c2f3759fdd2516fe2bd32b6c5f2533db9dafa
Aron <aron.cachago@gmail.com>
2023-08-10 15:00:47 -0500

    ngfor uso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+
+interface Entrada {
+  title: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +16,17 @@ export class AppComponent {
   protected registrado = false;
   protected nombre:string = "";
   protected apellido:string = "";
+  protected entradas:Entrada[];
+
+  constructor(){
+    this.entradas = [
+      {title:"Python cada dia mas presente"},
+      {title:"Java presente desde hace 20 decadas"},
+      {title:"Java Script el pilar"},
+      {title:"Kotlin cada vez mas potente"},
+      {title:"donde quedo pascal?"}
+    ]
+  }
 
 
   protected registrarUsuario(){
